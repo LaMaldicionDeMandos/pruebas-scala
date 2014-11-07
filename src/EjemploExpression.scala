daebe9570ac229a6e84577fbe9b3f6fd1ca5234d
Marcelo Pasut <pasutmarcelo@gmail.com>
2014-11-07 15:09:21 -0300

    primer ejemplo: recorre un arbol y aplica operaciones

---
+++ src/EjemploExpression.scala
@@ -0,0 +1,33 @@
+/**
+ * Created by marcelo on 11/7/14.
+ */
+abstract class Tree
+case class Sum(l: Tree, r:Tree) extends Tree;
+case class Product(l: Tree, r:Tree) extends Tree;
+case class Const(v: Int) extends Tree;
+case class Var(v: String) extends Tree;
+
+object EjemploExpression {
+  type Enviroment = String => Int;
+  def main(args: Array[String]): Unit = {
+
+    //a + b*c
+    val expression:Tree = Sum(Var("a"), Product(Var("b"), Var("c")));
+
+    val env: Enviroment = {case "a" => 2 case "b" => 3 case "c" => 5};
+    println("2 + 3*5 = "  + eval(expression, env));
+
+    val env1: Enviroment = {case "a" => 1 case "b" => 2 case "c" => 5};
+    println("1 + 2*5 = "  + eval(expression, env1));
+  }
+  def eval(tree:Tree, env:Enviroment): Int = tree match {
+    case Sum(l, r) => eval(l, env) + eval(r,env);
+    case Product(l, r) => eval(l, env) * eval(r,env);
+    case Const(v) => v;
+    case Var(n) => env(n);
+  }
+}
+
+
+
+
