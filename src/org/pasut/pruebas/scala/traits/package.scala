39403d22688b31e0f35a9955a1655b1122142b94
Marcelo Pasut <pasutmarcelo@gmail.com>
2014-12-05 12:30:02 -0300

    tipos compuestos

---
+++ src/org/pasut/pruebas/scala/traits/package.scala
@@ -0,0 +1,18 @@
+package org.pasut.pruebas.scala
+
+/**
+ * Los Traits son como interfaces, pero pueden implementar metodos
+ * Created by marcelo on 05/12/14.
+ */
+package object traits {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  //Puedo esperar un objeto que sea de dos tipos a la vez
+  def cloneAndCompare(obj: Cloneable with Comparable[AnyRef]): Cloneable = {
+    val copia: AnyRef = obj.clone()
+    obj.compareTo(copia)
+    obj
+  }
+}
