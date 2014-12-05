532964f5f8ff7bf2e57f8544c11e7e83f250ae5b
Marcelo Pasut <pasutmarcelo@gmail.com>
2014-12-05 15:41:14 -0300

    herencia multiple

--- src/org/pasut/pruebas/scala/traits/package.scala
+++ src/org/pasut/pruebas/scala/traits/package.scala
@@ -6,7 +6,9 @@ package org.pasut.pruebas.scala
  */
 package object traits {
   def main(args: Array[String]): Unit = {
-
+    class Iter extends StringIterator(args(0)) with RichIterator
+    val iter = new Iter
+    iter foreach println
   }
 
   //Puedo esperar un objeto que sea de dos tipos a la vez
@@ -15,4 +17,22 @@ package object traits {
 //    obj.compareTo(copia)
 //    obj
 //  }
+
+//  Puedo usar herencia multiple de la siguiente manera
+  abstract class AbsIterator {
+    type T
+    def hasNext: Boolean
+    def next: T
+  }
+
+  trait RichIterator extends AbsIterator {
+    def foreach(f: T => Unit) { while (hasNext) f(next) }
+  }
+
+  class StringIterator(s: String) extends AbsIterator {
+    type T = Char
+    private var i = 0
+    def hasNext = i < s.length()
+    def next = { val ch = s charAt i; i += 1; ch }
+  }
 }
